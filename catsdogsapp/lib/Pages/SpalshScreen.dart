51b1bc0a1c2e4bb10e1c62b5e8291b2b15766bcb
MokshaDill <mokshadil@gmail.com>
2023-12-19 10:26:37 +0530

    check 1

--- catsdogsapp/lib/Pages/SpalshScreen.dart
+++ catsdogsapp/lib/Pages/SpalshScreen.dart
@@ -1,4 +1,5 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:catsdogsapp/Pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
 class splashScreen extends StatefulWidget {
@@ -13,7 +14,7 @@ class _splashScreenState extends State<splashScreen> {
   Widget build(BuildContext context) {
     return FlutterSplashScreen(
       setStateTimer: Duration(microseconds: 20),
-      nextScreen: HomePage(),
+      nextScreen: homepage(),
       splashScreenBody: Text('Animal Classifier'),
       
 
